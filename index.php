0da40167c975dda3be592e08466e62a0dcaa85f8
Matho21 <espoirmatho21@gmail.com>
2016-02-13 15:57:54 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+/*
+*Ceci ce la page d'accueil avec le formulaire 
+*qui permettra à l'utilisateur de se connecter dans notre système
+*
+*/
+<form action="" metho="post">
+  <label>Pseudo ou identifiant:</label>
+  <input type="text" name="pseudo">
+
+  <label>Mot de passe:</label>
+  <input type="text" name="pseudo">
+  
+  
+  <input type="submit" value="Se connecter">
+
+</form>
